c95cd7e7e8fcab2e54d27c2997184c8959093647
Vitor Nogueira <vitor.nogueira94@gmail.com>
2022-12-08 02:00:14 -0300

    Desenvolvimento dos testes unitarios (JUnit)

---
+++ src/test/java/br/ce/wcaquino/taskbackend/utils/DataUtilsTest.java
@@ -0,0 +1,33 @@
+package br.ce.wcaquino.taskbackend.utils;
+
+import java.time.LocalDate;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+
+
+public class DataUtilsTest {
+	
+	@Test
+	public void deveRetornarTrueParaDatasFuturas() {
+		LocalDate date = LocalDate.of(2030, 01, 01);
+		Assert.assertTrue(DateUtils.isEqualOrFutureDate(date));
+	
+	}
+	
+	@Test
+	public void deveRetornarFalseParaDatasPassadas() {
+		LocalDate date = LocalDate.of(2010, 01, 01);
+		Assert.assertFalse(DateUtils.isEqualOrFutureDate(date));
+	
+	}
+	
+	@Test
+	public void deveRetornarTrueParaDataAtual() {
+		LocalDate date = LocalDate.now();
+		Assert.assertTrue(DateUtils.isEqualOrFutureDate(date));
+	
+	}
+
+}
